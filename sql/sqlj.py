c15c6e3c8007537a9dc465f9e2f8b19de3e2512b
Kevin Labtani <greenleaf1348@gmail.com>
2015-03-05 17:27:11 +0100

    Multiple Tables - Database Programming

---
+++ sql/sqlj.py
@@ -0,0 +1,16 @@
+# Joining data from multiple tables
+
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+    c = connection.cursor()
+
+    # get the data
+    c.execute("""SELECT population.city, population.population, regions.region
+                 FROM population, regions
+                 WHERE population.city = regions.city""")
+
+    rows = c.fetchall()
+
+    for r in rows:
+        print r[0], r[1], r[2]
