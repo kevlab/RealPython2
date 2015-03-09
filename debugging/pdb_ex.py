a6574679ad2ff951bc1ac64884f66c9d6f5ed589
Kevin Labtani <greenleaf1348@gmail.com>
2015-03-09 16:25:27 +0100

    Debugging - start debugging

--- debugging/pdb_ex.py
+++ debugging/pdb_ex.py
@@ -1,11 +1,13 @@
 import sys
 from random import choice
+import pdb
 
 random1 = range(1, 13)
 random2 = range(1, 13)
 
 while True:
     print "To exit this game type 'exit'"
+    pdb.set_trace()
     answer = raw_input("What is {} times {}? "
                        .format(choice(random1), choice(random2)))
 
