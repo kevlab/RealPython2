a6e6ad1de7a4a7cc161b5526613e9ecb661e3be2
Kevin Labtani <greenleaf1348@gmail.com>
2015-03-09 16:18:37 +0100

    Debugging - initialize

---
+++ debugging/pdb_ex.py
@@ -0,0 +1,19 @@
+import sys
+from random import choice
+
+random1 = range(1, 13)
+random2 = range(1, 13)
+
+while True:
+    print "To exit this game type 'exit'"
+    answer = raw_input("What is {} times {}? "
+                       .format(choice(random1), choice(random2)))
+
+    if answer == exit:
+        print "Now exiting game!"
+        sys.exit()
+
+    elif answer == choice(random2) * choice(random1):
+        print "Correct!"
+    else:
+        print "Wrong!"
