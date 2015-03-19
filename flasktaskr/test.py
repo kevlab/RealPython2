7631ccedd715257d5fe791b5f08a8ae110307f45
Kevin Labtani <greenleaf1348@gmail.com>
2015-03-19 20:19:24 +0100

    Flasktaskr - Testing login_form_presence

--- flasktaskr/test.py
+++ flasktaskr/test.py
@@ -24,11 +24,19 @@ class Alltests(unittest.TestCase):
     def tearDown(self):
         db.drop_all()
 
-
     def test_user_setup(self):
         new_user = User("klabtani", "emailad@gmail.com", "kevinlabtani")
         db.session.add(new_user)
         db.session.commit()
+        test = db.session.query(User).all()
+        for t in test:
+            t.name
+        assert t.name == "klabtani"
+
+    def test_form_is_present_on_login_page(self):
+        response = self.app.get('/')
+        self.assertEquals(response.status_code, 200)
+        self.assertIn('Please login to access your task list', response.data)
 
 if __name__ == "__main__":
     unittest.main()
