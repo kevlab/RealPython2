10587381867ac233408a05a188f8d4f65d90ec45
Kevin Labtani <greenleaf1348@gmail.com>
2015-03-25 17:01:52 +0100

    flaskTaskr - post migration to add user roles

--- flasktaskr/db_migrate.py
+++ flasktaskr/db_migrate.py
@@ -35,6 +35,6 @@ with sqlite3.connect(DATABASE_PATH) as connection:
                  FROM old_users
                  ORDER BY id ASC""")
     data = [(row[0], row[1], row[2], 'user') for row in c.fetchall()]
-    c.executemany("""INSERT INTO users(namen email, password, role)
+    c.executemany("""INSERT INTO users(name, email, password, role)
                      VALUES(?,?,?,?)""", data)
     c.execute("DROP TABLE old_users")
