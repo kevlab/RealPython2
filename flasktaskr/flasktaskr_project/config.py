1bf28c2fc272ba70a0c0bbc957535259b2bd7553
Kevin Labtani <greenleaf1348@gmail.com>
2015-03-26 18:17:40 +0100

    flaskTaskr - reorg the app with Blueprints

---
+++ flasktaskr/flasktaskr_project/config.py
@@ -0,0 +1,11 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+CSRF_ENABLED = True
+SECRET_KEY = 'insert_key_here'
+
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + DATABASE_PATH
