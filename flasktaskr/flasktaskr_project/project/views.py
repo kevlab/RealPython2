d0e7aaae6875b9b9d5a64281b4d1c2cfae2e7154
Kevin Labtani <greenleaf1348@gmail.com>
2015-03-31 17:38:37 +0200

    FlaskTaskr - add custom 500 error page

--- flasktaskr/flasktaskr_project/project/views.py
+++ flasktaskr/flasktaskr_project/project/views.py
@@ -25,3 +25,7 @@ def index(page):
 @app.errorhandler(404)
 def internal_error(error):
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
--- flasktaskr/flasktaskr_project/test_main.py
+++ flasktaskr/flasktaskr_project/test_main.py
@@ -12,6 +12,7 @@ class Alltests(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
             os.path.join(basedir, TEST_DB)
         self.app = app.test_client()
