c4ea15a0975bb2f5d6b67689b34e68e992f45642
Kevin Labtani <greenleaf1348@gmail.com>
2015-04-08 15:59:57 +0200

    flaskr-bdd add code and template for login and logout

--- flaskr-bdd/flaskr.py
+++ flaskr-bdd/flaskr.py
@@ -1,18 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for,\
+                  flash
 
 DATABASE = ''
-USERNAME = ''
-PASSWORD = ''
-SECRET_KEY = ''
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'notsosecret'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-
 
 @app.route("/")
 def hello():
     text = "Hello World!"
     return render_template('index.html', text=text)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
