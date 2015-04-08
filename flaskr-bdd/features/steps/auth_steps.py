191943303afc93106e6ba1782f22b94cd9fbf936
Kevin Labtani <greenleaf1348@gmail.com>
2015-04-08 15:47:48 +0200

    flaskr-bdd add tests for login and logout

--- flaskr-bdd/features/steps/auth_steps.py
+++ flaskr-bdd/features/steps/auth_steps.py
@@ -3,3 +3,21 @@ from behave import *
 @given(u'flaskr is setup')
 def flask_is_setup(context):
     assert context.client
+
+@given(u'we login with "{username}" and "{password}"')
+@when(u'we login with "{username}" and "{password}"')
+def login(context, username, password):
+    context.page = context.client.post('/login',
+                                       data=dict(username=username,
+                                                 password=password),
+                                       follow_redirects=True)
+    assert context.page
+
+@when(u'we logout')
+def logout(context):
+    context.page = context.client.get('/logout', follow_redirects=True)
+    assert context.page
+
+@then(u'we should see the alert "{message}"')
+def message(context, message):
+    assert message in context.page.data
