98e61c9a19d473b2bd157fda77f65ffa6534219e
Kevin Labtani <greenleaf1348@gmail.com>
2015-04-09 18:01:05 +0200

    flaskr-bdd behave for db

--- flaskr-bdd/features/environment.py
+++ flaskr-bdd/features/environment.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import tempfile
 
 # dd module to syspath
 pwd = os.path.abspath(os.path.dirname(__file__))
@@ -8,10 +9,17 @@ new_path = pwd.strip(project)
 full_path = new_path  # os.path.join(new_path, 'flaskr')
 
 try:
-    from flaskr import app
+    from flaskr import app, init_db
 except ImportError:
     sys.path.append(full_path)
-    from flaskr import app
+    from flaskr import app, init_db
 
 def before_feature(context, feature):
+    app.config['TESTING'] = True
+    context.db, app.config['DATABASE'] = tempfile.mkstemp()
     context.client = app.test_client()
+    init_db()
+
+def after_feature(context, feature):
+    os.close(context.db)
+    os.unlink(app.config['DATABASE'])
