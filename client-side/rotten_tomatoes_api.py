df766ad4cecb28d95b0e77b2e5dd75583cf9e2d8
Kevin Labtani <greenleaf1348@gmail.com>
2015-04-15 16:26:58 +0200

    web2py - rotten tomtes api update db

--- client-side/rotten_tomatoes_api.py
+++ client-side/rotten_tomatoes_api.py
@@ -6,4 +6,3 @@ with sqlite3.connect("movies.db") as connection:
     c.execute("""CREATE TABLE new_movies
               (title TEXT, year INT, rating TEXT, release TEXT, runtime INT,
                critics_review INT, audience_review INT)""")
-
