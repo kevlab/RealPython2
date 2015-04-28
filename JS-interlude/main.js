34c54ce5017b55682a46b264d30449ce77892500
Kevin Labtani <greenleaf1348@gmail.com>
2015-04-28 14:52:45 +0200

    JS interlude - finish

--- JS-interlude/main.js
+++ JS-interlude/main.js
@@ -6,6 +6,18 @@ $(function() {
         if ($('input').val() !== '') {
             var input = $("input").val()
             console.log(input)
+            // add value to DOM
+            $('ol').append('<li><a href="">x</a> - ' + input + '</li>');
         }
-    })
+        $('input').val('');
+    });
+
+});
+
+// remove text from DOM
+$(document).on('click', 'a', function (e) {
+    // cancel defult action of clicking the link
+    e.preventDefault();
+    // this refers to the current object, a, and we’re removing the parent element, <li>.
+    $(this).parent().remove();
 });
