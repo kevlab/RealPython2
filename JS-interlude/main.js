06971269e8444c0a66506ff219069b5c75955329
Kevin Labtani <greenleaf1348@gmail.com>
2015-04-28 14:41:18 +0200

    JS interlude - basic jquery writing to console

---
+++ JS-interlude/main.js
@@ -0,0 +1,11 @@
+$(function() {
+    console.log("whee!")
+
+    // event handler
+    $("#btn-click").click(function() {
+        if ($('input').val() !== '') {
+            var input = $("input").val()
+            console.log(input)
+        }
+    })
+});
