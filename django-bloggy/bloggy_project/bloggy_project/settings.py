b4e359c98c8f4a313cce7160c63c38ea4bb7688b
Kevin Labtani <greenleaf1348@gmail.com>
2015-05-01 14:25:11 +0200

    django - bloggy-project add media

--- django-bloggy/bloggy_project/bloggy_project/settings.py
+++ django-bloggy/bloggy_project/bloggy_project/settings.py
@@ -82,7 +82,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, 'templates'),
