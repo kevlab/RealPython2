a3a5b95093fd31c70789804a2d5d848023c3a26d
Kevin Labtani <greenleaf1348@gmail.com>
2015-05-04 16:55:57 +0200

    django - bloggy-project popular posts on single post view

--- django-bloggy/bloggy_project/blog/views.py
+++ django-bloggy/bloggy_project/blog/views.py
@@ -25,6 +25,9 @@ def post(request, post_url):
     single_post = get_object_or_404(Post, title=post_url.replace('_', ' '))
     single_post.views += 1
     single_post.save()
+    popular_posts = Post.objects.order_by('-views')[:5]
+    for popular_post in popular_posts:
+        popular_post.url = encode_url(popular_post.title)
     t = loader.get_template('blog/post.html')
-    c = Context({'single_post': single_post, })
+    c = Context({'single_post': single_post, 'popular_posts': popular_posts})
     return HttpResponse(t.render(c))
