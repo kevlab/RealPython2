fe858a78934eb84e68454603ef16e8fbe0cd4ae6
Kevin Labtani <greenleaf1348@gmail.com>
2015-05-05 17:26:11 +0200

    django - bloggy-project forms

--- django-bloggy/bloggy_project/blog/views.py
+++ django-bloggy/bloggy_project/blog/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import Context, loader, RequestContext
 
 from blog.models import Post
+from blog.forms import PostForm
+
 
 def encode_url(url):
     return url.replace(' ', '_')
+
 
 def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
@@ -31,3 +34,17 @@ def post(request, post_url):
     t = loader.get_template('blog/post.html')
     c = Context({'single_post': single_post, 'popular_posts': popular_posts})
     return HttpResponse(t.render(c))
+
+
+def add_post(request):
+    context = RequestContext(request)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect(index)
+        else:
+            print form.errors  # display errors to enduser
+    else:
+        form = PostForm()
+    return render_to_response('blog/add_post.html', {'form': form}, context)
