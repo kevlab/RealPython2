330746891a0afea92e726a44c6d2c1db57ed745d
Kevin Labtani <greenleaf1348@gmail.com>
2015-05-06 15:49:52 +0200

    django - bloggy-project slugs for better urls handling

--- django-bloggy/bloggy_project/blog/urls.py
+++ django-bloggy/bloggy_project/blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns(
                 'blog.views',
                 url(r'^$', views.index, name='index'),
                 url(r'^add_post/', views.add_post, name='add_post'),
-                url(r'^(?P<post_url>\w+)/$', views.post, name='post'),
+                url(r'^(?P<slug>[\w|\-]+)/$', views.post, name='post'),
                 )
